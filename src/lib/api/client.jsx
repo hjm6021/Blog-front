c5ad76a8a00964c6e52ab8b035d3a2e435043c0f
JMHan <hjm6021@gmail.com>
2022-07-30 16:48:44 +0900

    change url

--- src/lib/api/client.jsx
+++ src/lib/api/client.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const client = axios.create();
-client.defaults.baseURL = 'http://localhost:5000';
+client.defaults.baseURL = 'http://www.jmhan-blog.com/';
 
 export default client;
